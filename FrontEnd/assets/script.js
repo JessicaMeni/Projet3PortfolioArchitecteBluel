8c5037203f09b87d8bc075e936b9b87165a63a58
Jessica M <jessica-95210@hotmail.fr>
2024-04-20 02:42:58 +0200

    Creations des boutons filtres et evenements click

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -6,46 +6,70 @@ console.log("EN TITRE PROJETS", projets);
 //const premierProjet = projets[0];
 //console.log("EN TITRE PREMIER PROJET", premierProjet);
 //console.log("EN TITRE MON PREMIER PROJET", premierProjet.title);
-//faire une boucle en html avec une figure, une img, et le titre en JS
 
+const sectionGallery = document.querySelector(".gallery"); //peut être dehors pour ne pas la créer à chaque fois
 
 for (let i = 0; i < projets.length; i++) {
     
     const article = projets[i]; 
 
-    const sectionGallery = document.querySelector(".gallery");
     const baliseFigure = document.createElement("figure");
-    //baliseFigure.innerText = article.figure; //ça ne sert a rien car figure n'existe pas dans le work ?
-    //let parentElement = document.getElementById("gallery"); (Question : c correct ? non,car gallery n'est pas id mais une balise)
-    //c pour mettre figure dans <gallery> ?
-    sectionGallery.appendChild(baliseFigure); // on ajoute l'enfant <figure> à <.gallery>
-
-
+    //baliseFigure.innerText = ``; //ça peut fonctionner mais remplacer innerText par innerHTML puis mettre directement dedans balise img et figcaption
+    //let parentElement = document.getElementById("gallery"); (Question : c correct ? non,car gallery n'est pas n'a pas d'id mais c une balise)
+    
     const baliseImage = document.createElement("img");
-    baliseImage.src = article.imageUrl; //m'affiche 11 elements quand je remplace article par projets
+    baliseImage.src = article.imageUrl;
     const baliseFigcaption = document.createElement("figcaption");
-    baliseFigcaption.innerText = article.title; //(à la place de innerText, textContent ?)
+    baliseFigcaption.innerText = article.title; //à la place de innerText, textContent ? oui c la meme chose mais c + trop utilisé
 
-    const sectionFigure = document.querySelector(".gallery", "figure"); //pas de . devant figure? pq je ne peux pas mettre baliseFigure
-    sectionFigure.appendChild(baliseImage);
-    sectionFigure.appendChild(baliseFigcaption); // je dois en faire deux pour intégrer baliseFigcaption sinon ça ne fonctionne pas
-
-//sectionFigure.appendChild(baliseImage, baliseFigcaption);
-} // et le ALT dans <img?>
+    //nope pas utile const sectionFigure = document.querySelector(".gallery", "figure"); // pq je ne peux pas mettre baliseFigure a la place de"figure" ?. querySelector selectionne le 1er gallery figure qu'il y a
+    baliseFigure.appendChild(baliseImage); // utiliser baliseFigure directement
+    baliseFigure.appendChild(baliseFigcaption);
+    
+    sectionGallery.appendChild(baliseFigure); // on ajoute l'enfant <figure> à <.gallery> Ronan le mets en bas pour de la micro optimisation
+} // et le ALT dans <img?> on peut l'ajouter avec createElement qui reprend .title
 
 const reponseDeux = await fetch("http://localhost:5678/api/categories");
 const catego = await reponseDeux.json();
-
 console.log("TYPE DE CATEGORIES", catego);
 
-const baliseBoutonObjets = document.createElement("button");
-baliseBoutonObjets.textContent = "Objets"
+const Sectionfilterchoix = document.querySelector(".filterchoix")
 
-const sectionGalleryFiltres = document.querySelector(".gallery"); //je dois recréer une const qui lie de nouveau a gallery sinon ça ne fonctionne pas
-sectionGalleryFiltres.appendChild(baliseBoutonObjets);
+for (let i = 0; i < catego.length; i++) {
 
-const boutonObjets = document.querySelector(".button");
-boutonObjets.addEventListener("click", function () {
-    ?
+    const filtres = catego[i];
+    
+    const baliseBoutonObjets = document.createElement("button"); 
+    baliseBoutonObjets.innerText = filtres.name;
+    baliseBoutonObjets.className = filtres.name;//la class Hotel & ... pb à cause du & ?
+    
+    Sectionfilterchoix.appendChild(baliseBoutonObjets);
+}
+
+const FiltreObjets = document.querySelector(".Objets");
+
+FiltreObjets.addEventListener("click", function () {
+    const filtreQueDesObjets = projets.filter(function (truc) {
+     return truc.categoryId === 1;
+    })
+    console.log(filtreQueDesObjets)
+});
+//for (let i = projets.length -1 ; i >= 0; i--) {// hein? 
+//    if(filtreQueDesObjets !== 1)
+//}
+
+const FiltreAppartements = document.querySelector(".Appartements");
+FiltreAppartements.addEventListener("click", function () {
+    const filtreQueDesAppartements = projets.filter(function (truc) {
+     return truc.categoryId === 2;
+    })
+    console.log(filtreQueDesAppartements)
 });
 
+const FiltreHotelsResto = document.querySelector(".Hotels"); //je n'ai pas pu mettre &
+FiltreHotelsResto.addEventListener("click", function () {
+    const filtreQueDesHotelsEtRestaur = projets.filter(function (truc) {
+     return truc.categoryId === 3;
+    })
+    console.log(filtreQueDesHotelsEtRestaur)
+});
